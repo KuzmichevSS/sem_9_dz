a5d40b6b01b2d41f27dc9c7bafb5b197a3dd458e
Sergey Kuzmichev <kyz1979@mail.ru>
2023-02-27 04:44:48 +0300

    add z 66

---
+++ z-66/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("M = ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("N = ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"{SumNumbers(M, N)}");
+ 
+static int SumNumbers(int M, int N)
+{
+    if (M == 0) return (N * (N + 1)) / 2;
+    else if (N == 0) return (M * (M + 1)) / 2;
+    else if (M == N) return M;
+    else if (M < N) return N + SumNumbers(M, N - 1);
+    else return N + SumNumbers(M, N + 1);
+}
